51cad43b718ac688d405c920fa24633e818ebfb9
Spote777 <66115092+Spote777@users.noreply.github.com>
2023-01-18 13:04:09 +0300

    Initial Commit

---
+++ MusicPlaylist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlaylist
+//
+//  Created by Павел Заруцков on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
